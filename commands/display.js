a2ba6541b93dca56a61fadb2980c9d0b6236820e
wojin57 <wojin57@gmail.com>
2022-08-21 17:14:58 +0900

    DISPLAY embed complete
    MANAGE still has bugs, need to fix

--- commands/display.js
+++ commands/display.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { getGameChannels, getRequests } = require("../utils.js");
 
 module.exports = {
@@ -9,14 +9,40 @@ module.exports = {
         const gameChannelNames = getGameChannels().map(
             (gameChannel) => gameChannel.channel.name
         );
-        const requestsToString = getRequests().map(
-            (request) =>
-                `${request.channel_name} - ${
-                    request.role_name
-                } - ${request.members.map((member) => member.displayName)}`
-        );
-        await interaction.reply(
-            `채널 목록: ${gameChannelNames}\n요청 목록: ${requestsToString}`
-        );
+
+        let requestsToString = getRequests()
+            .map(
+                (request) =>
+                    `${request.channel_name} - ${
+                        request.role_name
+                    } - ${request.members.map((member) => member.displayName)}`
+            )
+            .join("\n");
+
+        if (!requestsToString) {
+            requestsToString = "없음";
+        }
+
+        let fields = [];
+        const maxPage = Math.floor(gameChannelNames.length / 25) + 1;
+        for (let i = 0; i < maxPage; i++) {
+            const numChannels =
+                i === maxPage - 1 ? gameChannelNames.length % 25 : 25;
+            fields.push({
+                name: `채널 목록 (페이지 ${i + 1}/${maxPage})`,
+                value: gameChannelNames
+                    .slice(i * 25, i * 25 + numChannels)
+                    .join(", "),
+                inline: false,
+            });
+        }
+        fields.push({
+            name: "요청 목록",
+            value: requestsToString,
+            inline: false,
+        });
+        const displayEmbed = new EmbedBuilder().addFields(fields);
+
+        await interaction.reply({ embeds: [displayEmbed] });
     },
 };
--- commands/test.js
+++ commands/test.js
@@ -3,77 +3,39 @@ const {
     ButtonBuilder,
     ButtonStyle,
     ActionRowBuilder,
+    EmbedBuilder,
 } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("테스트")
-        .setDescription("테스트용 명령어입니다.")
-        .addIntegerOption((option) =>
-            option
-                .setName("pagenumber")
-                .setDescription("Enter the page number")
-                .setRequired(true)
-                .setMinValue(1)
-                .setMaxValue(5)
-        ),
+        .setDescription("테스트용 명령어입니다."),
     async execute(interaction) {
-        const pageNumber = interaction.options.getInteger("pagenumber");
-
-        const buttons = [
+        const value = "Some value here";
+        const fields = [
+            { name: "Regular field title", value: "Some value here" },
+            { name: "\u200B", value: "\u200B" },
             {
-                customId: "yeah",
-                label: "yes",
-                style: ButtonStyle.Success,
-                action: async (interaction) => {
-                    await interaction.reply("You selected yes");
-                },
+                name: "Inline field title",
+                value: value,
+                inline: true,
             },
             {
-                customId: "nope",
-                label: "no",
-                style: ButtonStyle.Danger,
-                action: async (interaction) => {
-                    await interaction.reply("You selected no");
-                },
+                name: "Inline field title",
+                value: value,
+                inline: true,
             },
         ];
-
-        const buttonRow = new ActionRowBuilder().addComponents(
-            buttons.map((button) => {
-                return new ButtonBuilder()
-                    .setCustomId(button.customId)
-                    .setLabel(button.label)
-                    .setStyle(button.style);
-            })
-        );
-
-        await interaction.reply({
-            content: `Button select #${pageNumber}`,
-            components: [buttonRow],
-        });
-
-        const buttonFilter = (interaction) => {
-            return buttons.filter(
-                (button) => button.customId === interaction.customId
-            );
-        };
-
-        const buttonCollector =
-            interaction.channel.createMessageComponentCollector({
-                filter: buttonFilter,
-                time: 60 * 1000,
+        const exampleEmbed = new EmbedBuilder()
+            .setTitle("Some title")
+            .setDescription("Some description here")
+            //.addFields(fields)
+            .addFields({
+                name: "Inline field title",
+                value: "Some value here",
+                inline: true,
             });
 
-        buttonCollector.on("collect", async (interaction) => {
-            const button = buttons.find(
-                (button) => button.customId === interaction.customId
-            );
-            await button.action(interaction);
-        });
-
-        buttonCollector.on("end", async (collect) => {
-            console.log("timeout!");
-        });
+        await interaction.reply({ embeds: [exampleEmbed] });
     },
 };
